15df06cedd8b25be71cede4e5476543451596737
ryryrymln88808 <ryryrymln88808@gmail.com>
2024-06-23 12:02:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "21291")
-    API_HASH = environ.get("API_HASH", "ed3c9220b55911903f3fb61f3")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6558508467Fy7bWSpD-2qd-LPH9tAnL2GW8T1Qe4") 
+    API_ID = environ.get("API_ID", "21701469")
+    API_HASH = environ.get("API_HASH", "ae456165cc04a86ee38bd8ced7dae0e3")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6907596900:AAF-GGlBBcm1WWAF1ekm-nYcO1jwq-oV_H0") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = "Paste Here Your MangoDB Url"
+    DATABASE_URI = "mongodb+srv://rasy46765:uxDpwzp7DDHIKnCj@cluster0.rakmbj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DATABASE_NAME = environ.get("DATABASE_NAME", "MswForwardBot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6001039737').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1557042262').split()]
 
 class temp(object): 
     lock = {}
